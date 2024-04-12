93384167a06e8d1426ef8c16754f9b1550b4a993
kodezero1 <samadeol03@gmail.com>
2024-04-12 16:43:07 +0100

    Add account recovery logic

--- frontend/src/pages/RecoverAccount/RecoverAccount.jsx
+++ frontend/src/pages/RecoverAccount/RecoverAccount.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BulbOutlined } from "@ant-design/icons";
+import { Button, Input } from "antd";
+import { ethers } from "ethers";
 
-export default function RecoverAccount() {
+const { TextArea } = Input;
+export default function RecoverAccount({ setWallet, setSeedPhrase }) {
+  const navigate = useNavigate();
+  const [typedSeed, setTypedSeed] = useState("");
+  const [nonValid, setNonValid] = useState(false)
+
+  const seedAdjust = (e) => {
+    setWallet(false)
+    setTypedSeed(e.target.value);
+  };
+
+  const recoverWallet = () => {
+    let recoverableWallet
+    try {
+      recoverableWallet = ethers.Wallet.fromPhrase(typedSeed)
+    } catch (error) {
+      setNonValid(true)
+      return
+    }
+    setSeedPhrase(typedSeed)
+    setWallet(recoverableWallet.address)
+    navigate("/yourwallet")
+    return
+  }
   return (
-    <div>
-      Recover Account
+    <div className="content">
+      <div className="mnemonic">
+        <BulbOutlined style={{ fontSize: "20px" }} />
+        <div>
+          Type your seed phrase in the field below to recover your wallet
+        </div>
+      </div>
+      <TextArea
+        value={typedSeed}
+        onChange={seedAdjust}
+        rows={4}
+        placeholder="Enter your seed phrase here"
+        className="seedPhraseContainer"
+      />
+      <Button
+        disabled={typedSeed.split(" ").length !== 12 || typedSeed.slice(-1) === " " }
+        className="frontPageButton"
+        type="primary"
+        onClick={() => recoverWallet()}
+      >
+        Recover wallet
+      </Button>
+      {nonValid && <p style={{color: "red"}}>Invalid seed phrase</p>}
+      <p className="frontPageButton" onClick={() => navigate("/")}>
+        Go back
+      </p>
     </div>
-  )
+  );
 }
--- frontend/src/pages/WalletView/WalletView.jsx
+++ frontend/src/pages/WalletView/WalletView.jsx
@@ -1,37 +1,65 @@
-import { useState, useEffect } from 'react'
-import { Divider} from 'antd'
+import { useState, useEffect } from "react";
+import {
+  Divider,
+  Tooltip,
+  List,
+  Avatar,
+  Spin,
+  Tabs,
+  Input,
+  Button,
+} from "antd";
+import { LogoutOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
-export default function WalletView({wallet, setWallet, seedPhrase, setSeedPhrase}) {
-    const tokens = [
-        {
-            symbol: "Eth",
-            name: "Ethereum",
-            balance: 10000,
-            decimals: 18,
-        },
-        {
-            symbol: "Link",
-            name: "Chainlink",
-            balance: 10000,
-            decimals: 18,
-        },
-        {
-            symbol: "UNI",
-            name: "Uniswap",
-            balance: 10000,
-            decimals: 18,
-        },
-        {
-            symbol: "MATIC",
-            name: "Polygon",
-            balance: 10000,
-            decimals: 18,
-        }
-    ]
-    // console.log(wallet)
-    return (
-    <div className='content'>
-    {wallet}
+export default function WalletView({
+  wallet,
+  setWallet,
+  seedPhrase,
+  setSeedPhrase,
+}) {
+  const tokens = [
+    {
+      symbol: "Eth",
+      name: "Ethereum",
+      balance: 10000,
+      decimals: 18,
+    },
+    {
+      symbol: "Link",
+      name: "Chainlink",
+      balance: 10000,
+      decimals: 18,
+    },
+    {
+      symbol: "UNI",
+      name: "Uniswap",
+      balance: 10000,
+      decimals: 18,
+    },
+    {
+      symbol: "MATIC",
+      name: "Polygon",
+      balance: 10000,
+      decimals: 18,
+    },
+  ];
+
+  const navigate = useNavigate()
+  const nfts = [];
+
+  const logout = () => {
+    setSeedPhrase(null)
+    setWallet(null)
+    navigate("/")
+  }
+
+  return (
+    <div className="content">
+        <div className="logoutButton" onClick={logout}>
+            <LogoutOutlined/>
+        </div>
+        {wallet}
     </div>
   )
 }
