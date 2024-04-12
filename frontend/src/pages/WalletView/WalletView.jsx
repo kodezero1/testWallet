11479deda0b27d9aada884adfffedca31ad50287
kodezero1 <samadeol03@gmail.com>
2024-04-12 18:59:37 +0100

    Add nfts, tokens & transfer tabs

--- frontend/src/pages/WalletView/WalletView.jsx
+++ frontend/src/pages/WalletView/WalletView.jsx
@@ -11,6 +11,7 @@ import {
 } from "antd";
 import { LogoutOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
+import logo from "../../assets/noImg.png"
 
 export default function WalletView({
   wallet,
@@ -45,21 +46,106 @@ export default function WalletView({
     },
   ];
 
-  const navigate = useNavigate()
   const nfts = [];
+  const navigate = useNavigate();
+  const items = [
+    {
+      key: "3",
+      label: "Tokens",
+      children: (
+        <>
+          {tokens ? (
+            <>
+              <List
+                bordered
+                itemLayout="horizontal"
+                dataSource={tokens}
+                renderItem={(item, index) => (
+                  <List.Item style={{ textAlign: "left" }}>
+                    <List.Item.Meta
+                      avatar={<Avatar src={item.logo || logo} />}
+                      title={item.symbol}
+                      description={item.name}
+                    />
+                    <div>
+                      {(
+                        Number(item.balance) /
+                        10 ** Number(item.decimals)
+                      ).toFixed(2)}{" "}
+                      Tokens
+                    </div>
+                  </List.Item>
+                )}
+              />
+            </>
+          ) : (
+            <>
+              <span>You don't have any tokens yet</span>
+              <p className="frontPageButton"></p>
+            </>
+          )}
+        </>
+      ),
+    },
+    {
+      key: "2",
+      label: "NFTs",
+      children: (
+        <>
+          {nfts ? (
+            <>
+              {nfts.map((e, i) => {
+                return (
+                  <>
+                    {e && (
+                      <img
+                        key={i}
+                        className="nftImage"
+                        alt="nftImage"
+                        src={e}
+                      />
+                    )}
+                  </>
+                );
+              })}
+            </>
+          ) : (
+            <>
+              <span>You seem to not have any NFTs yet</span>
+              <p className="frontPageBottom">
+                
+              </p>
+            </>
+          )}
+        </>
+      ),
+    },
+    {
+      key: "1",
+      label: "Transfer",
+      children: <>Transfer</>,
+    },
+  ];
 
   const logout = () => {
-    setSeedPhrase(null)
-    setWallet(null)
-    navigate("/")
-  }
+    setSeedPhrase(null);
+    setWallet(null);
+    navigate("/");
+  };
 
   return (
     <div className="content">
-        <div className="logoutButton" onClick={logout}>
-            <LogoutOutlined/>
+      <div className="logoutButton" onClick={logout}>
+        <LogoutOutlined />
+      </div>
+      <div className="walletName">Wallet</div>
+      <Tooltip title={wallet}>
+        <div>
+          {wallet.slice(0, 4)}...{wallet.slice(38)}
         </div>
-        {wallet}
+      </Tooltip>
+      <Divider />
+      <Tabs defaultActiveKey="1" items={items} className="walletView" />
     </div>
-  )
+  );
 }
